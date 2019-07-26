c9ebcdaf40bc27789185e60e4e8b8168afc8546d
hg2355 <hg2355@github.com>
2019-07-26 15:13:23 -0500

    changed to goals

--- src/index.js
+++ src/index.js
@@ -2,77 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          value: null,
-        };
-      }
-
-    render() {
-      return (
-        <button 
-            className="square"   
-            onClick={() => this.setState({value: 'X'})}
-        >
-         {this.state.value}
-        </button>
-      );
-    }
-  }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i}/>;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
+function GoalBox(props) {
+  return (
+    <button id={props.value} className="goal-button" onClick={props.onClick}>
+      
+    </button>
   );
-  
+}
+
+function GoalDetails(props) {
+  return (
+    <div>{props.goal_detail}</div>
+  )
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      goals: Array(9).fill(null),
+      goal_detail: null
+    };
+  }
+
+  handleClick(i) {
+    this.setState({
+      goal_detail: "goal detail for goal " + i
+    });
+  }
+
+  renderGoal(i) {
+    return (
+      <GoalBox
+        key={i}
+        value={i}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    // Use two loops to make the squares
+    const num_goals = 6;
+    let goals = [];
+    for(let i=0; i<num_goals; ++i) {
+      let row = [];
+      row.push(this.renderGoal(i));  
+      goals.push(<div key={i} className="goal-row">{row}</div>);
+    }
+
+    return (
+      <div>
+        <GoalDetails 
+        goal_detail= {this.state.goal_detail} 
+        />
+        <div>{goals}</div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="goal-board">
+        <Board />
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
